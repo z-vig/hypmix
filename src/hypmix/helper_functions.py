df519552b28903cec6d980af8d88eb7b53ff9b5a
Z.M. Vig <zvig@umd.edu>
2025-12-19 13:00:07 -0500

    Added docstrings

--- src/hypmix/helper_functions.py
+++ src/hypmix/helper_functions.py
@@ -1,9 +1,18 @@
 from PySide6.QtWidgets import QApplication
 from hypmix.mixview.main_window import MixView
 import sys
+from pathlib import Path
 
 
-def open_mixview():
+def open_mixview(model_path: str | Path | None = None) -> None:
+    """
+    Convenience function for opening the main MixView GUI.
+
+    Parameters
+    ----------
+    model_path: str or Path, optional, default=None
+        File path to a model result file (.HDF5 format)
+    """
     app = QApplication(sys.argv)
     main = MixView()
     main.show()
